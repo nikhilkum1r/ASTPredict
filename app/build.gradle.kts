616f8c68856ded043377beea9d39e0affc3a08c9
Nikhil Kumar <nikhilkumar@users.noreply.github.com>
2026-05-27 13:04:14 +0530

    chore: bump version to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.astpredict.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
